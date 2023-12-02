79b2267c95dcc6fa6efecb31d38201f7df67164d
rave <34344235+Raveshaww@users.noreply.github.com>
2023-12-02 01:45:55 -0800

    feat: day2 solutions

---
+++ p2/CubeConudrum2/CubeConudrum2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.RegularExpressions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var promptInput = File.ReadAllLines("input.in").ToArray();
+        int sum = 0;
+        Regex[] colorsRx =
+        {
+            new(@"\b(\d+)\b\sred"),
+            new(@"\b(\d+)\b\sblue"),
+            new(@"\b(\d+)\b\sgreen")
+        };
+
+        foreach (string prompt in promptInput)
+        {
+            var minDiceRequired = new List<int>();
+
+            foreach (var color in colorsRx)
+            {
+                var rxResult = color.Matches(prompt);
+                if (rxResult.Count() > 0)
+                {
+                    int colorMin = 0;
+                    for (int i = 0; i < rxResult.Count; i++) 
+                    {
+                        int colorNumber = int.Parse(rxResult[i].Groups[1].Value);
+
+                        if (colorNumber > colorMin)
+                        {
+                            colorMin = colorNumber;
+                        }
+                    }
+                    minDiceRequired.Add(colorMin);
+                }
+            }
+
+            sum += minDiceRequired[0] * minDiceRequired[1] * minDiceRequired[2];
+        }
+        Console.WriteLine(sum);
+    }
+}
---
+++ p2/CubeConundrum/CubeConundrum/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var promptInput = File.ReadAllLines("input.in").ToArray();
+        int sum = 0;
+        Regex[] colorsRx =
+        {
+            new(@"\b(1[3-9]|[2-9]\d+)\b red"),
+            new(@"\b(1[4-9]|[2-9]\d+)\b green"),
+            new(@"\b(1[5-9]|[2-9]\d+)\b blue")
+        };
+
+        foreach (string prompt in promptInput)
+        {
+            bool validGame = true;
+
+            foreach (var color in colorsRx)
+            {
+                if (color.Matches(prompt).Count() >= 1)
+                {
+                    validGame = false;
+                    break;
+                }
+            }
+
+            if (validGame)
+            {
+                sum += int.Parse(prompt.Split(":")[0].Split(" ")[1]);
+            }
+        }
+        Console.WriteLine(sum);
+    }
+}
